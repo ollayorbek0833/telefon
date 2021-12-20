ae22a7e7e0b87f9ba716e31b66bc7952654e375d
ollayorbek0833 <ollayorbek2005@gmail.com>
2021-12-20 15:24:23 +0500

    Update screen.dart

--- lib/screen.dart
+++ lib/screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Screen extends StatefulWidget {
-  const Screen({Key? key}) : super(key: key);
+  const Screen({Key? key,}) : super(key: key);
 
 
   @override
@@ -11,16 +11,50 @@ class Screen extends StatefulWidget {
 class _ScreenState extends State<Screen> {
   @override
   Widget build(BuildContext context) {
-    String text = "";
+    String text = "dsa";
     return SafeArea(
       child: Scaffold(
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(text, style: const TextStyle(fontWeight: FontWeight.w600),),
-            const Text("Add number",style: TextStyle(color: Colors.blue),)
+            Text(text, textScaleFactor: 2,),
+            const Text("Add number",style: TextStyle(color: Colors.blue),),
+            Row(
+              children: [
+                TextButton(
+                  onPressed: (){
+                      text = text + "1";
+                      print(text);
+                  },
+                  child: const Text("1"),
+                )
+              ],
+
+            ),
           ],
         ),
       ),
     );
+  }
+}
+
+class NumberButton extends StatefulWidget {
+  NumberButton({Key? key, required this.number, required this.numbers}) : super(key: key);
+  int number;
+  String numbers;
+
+  @override
+  _NumberButtonState createState() => _NumberButtonState();
+}
+
+class _NumberButtonState extends State<NumberButton> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: (){
+        widget.numbers + widget.number.toString();
+      },
+      child: Text(widget.number.toString(), textScaleFactor: 2.5,),
+    );;
   }
 }
